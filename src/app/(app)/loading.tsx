68829ac6dc1b58ebf38c78ed7f38613457d23cf5
Hcelle <hucelle@gmail.com>
2026-08-05 19:55:49 +0200

    Temporaire : reintroduit le squelette pour verifier que les tests l attrapent

---
+++ src/app/(app)/loading.tsx
@@ -0,0 +1,5 @@
+// Fichier temporaire : reproduit l'incident #44 pour vérifier que les
+// parcours connectés le détectent. Sera retiré avant le merge.
+export default function Loading() {
+  return <p>Chargement…</p>;
+}
